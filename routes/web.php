08395b26656f549a53c503f0db61646c89fc7438
Jeú Chaves <jeuchaveslima@gmail.com>
2023-10-24 12:01:31 -0400

    vou continuar depois

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [ProdutoController::class, 'index']);
