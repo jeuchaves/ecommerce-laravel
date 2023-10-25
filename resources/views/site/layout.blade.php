04c1341e27e19c3ea349361af5fa2aa7793d529f
Jeú Chaves <jeuchaveslima@gmail.com>
2023-10-25 15:17:19 -0400

    feature: adição do menu de categorias

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -8,16 +8,31 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+        @foreach ($categoriasMenu as $categoriaMenu)
+            <li><a href="#!">{{$categoriaMenu->nome}}</a></li>
+        @endforeach
+    </ul>
+
     <nav class="red">
         <div class="nav-wrapper container">
             <a href="#" class="brand-logo center">Curso Laravel</a>
             <ul id="nav-mobile" class="left">
                 <li><a href="#">Home</a></li>
+                <li><a href="#" class="dropdown-trigger" data-target='dropdown1'>Categorias <i class="material-icons right">expand_more</i></a></li>
                 <li><a href="#">Carrinho</a></li>
             </ul>
         </div>
     </nav>
     @yield('conteudo')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script>
+        var elemDrop = document.querySelectorAll('.dropdown-trigger');
+        var instanceDrop = M.Dropdown.init(elemDrop, {
+            coverTrigger: false,
+            constrainWidth: false
+        });
+    </script>
 </body>
 </html>
